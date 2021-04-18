64d2a8454f09ea5ae2a01b5986256f4cfa92285a
Ryan Walmsley <ryan@ryanteck.uk>
2021-04-18 21:59:38 +0100

    python program to set UPNP once the miner is operational.

---
+++ configureUpnp.py
@@ -0,0 +1,25 @@
+# Python script to configure UPNP
+
+import os
+
+regionID = None
+while(regionID is None):
+    # While no region set
+    try:
+        with open("/var/pktfwd/region", 'r') as regionOut:
+            regionFile = regionOut.read()
+
+            if(len(regionFile) > 3):
+                print("Frequency: " + str(regionFile))
+                regionID = str(regionFile).rstrip('\n')
+                break
+        print("Invalid Contents")
+        sleep(60)
+        print("Try loop again")
+    except FileNotFoundError:
+        print("File Not Detected, Sleeping")
+        sleep(60)
+
+print("Configuring Upnp")
+
+os.system('upnpc -e "Nebra Helium" -r 44158 TCP')
